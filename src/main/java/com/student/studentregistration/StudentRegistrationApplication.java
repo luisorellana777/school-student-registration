531a9c2b17e3e700872b5bccd457ef283af767cf
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2021-06-23 22:42:02 -0400

    Initial Commit

---
+++ src/main/java/com/student/studentregistration/StudentRegistrationApplication.java
@@ -0,0 +1,18 @@
+package com.student.studentregistration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/com/student/studentregistration/StudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
